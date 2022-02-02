1b2812f9d578c108e229d705c3b979145cd057c3
Anthony Nunes <anthonytx22nunes@gmail.com>
2022-02-01 23:58:54 -0300

    Adding events to the buttons

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -1,6 +1,7 @@
 class CalcController {
     constructor() {
         // if an var beggins with an undeline '_' is supposed to be a private attribute
+        this._operation = [];
         this._locale = 'pt-BR';
         this._displayCalcEl = document.querySelector("#display");
         this._dateEl = document.querySelector("#data");
@@ -8,15 +9,74 @@ class CalcController {
         this._currentDate = new Date();
 
         this.initialize();
+        this.initButtonsEvents();
+        
     }
 
     initialize() {
+        this.setDisplayDateTime();
         setInterval(() => {
-            this.displayDate = this._currentDate.toLocaleDateString(this._locale);
-            this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
+            this.setDisplayDateTime();
         }, 1000);
-
     }
+
+    /**
+    * This function aims to get an DOM element or an array of then add
+    * events into it
+    * 
+    * Made by Tony
+    */
+    addEventListenerAll(element, events, fn)
+    {
+        events.split(' ').forEach(event =>{
+            element.addEventListener(event, fn, false);
+        });
+    }
+
+    clearAll()
+    {
+        
+    }
+
+    execBtn(buttonValue)
+    {   
+        switch(buttonValue)
+        {
+            case 'ac':
+                this.clearAll();
+                break;
+            
+        }
+    }
+
+    initButtonsEvents()
+    {
+        let buttons = document.querySelectorAll("#buttons > g, #parts > g");
+
+        buttons.forEach((btn, index) => {
+            this.addEventListenerAll(btn, 'click drag', (e) =>{
+                let textBtn = btn.className.baseVal.replace("btn-", "");
+
+                this.execBtn(textBtn);
+            });
+
+            this.addEventListenerAll(btn, 'mouseover mouseup mousedown', e =>{
+                btn.style.cursor = "pointer";
+            });
+        });
+    }
+
+    setDisplayDateTime()
+    {
+        this.displayDate = this._currentDate.toLocaleDateString(this._locale, { 
+            // these can be changed to other formats
+            day: "2-digit", 
+            month: "2-digit", 
+            year: "numeric"});
+        this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
+    }
+
+
 
     get displayTime() { return this._timeEl.innerHTML; }
     set displayTime(value) { return this._timeEl.innerHTML = value; }
